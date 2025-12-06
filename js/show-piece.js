98534788b63cba6e1dbe76559f3235d498b17e76
Lucas-132 <lucas.pollet135@gmail.com>
2025-12-06 19:44:54 +0100

    Added a button on piece infos page which redirect to version creation page

--- js/show-piece.js
+++ js/show-piece.js
@@ -1,22 +1,28 @@
+let params = new URLSearchParams(window.location.search);
+
 let stringPieces = localStorage.getItem("pieces");
 let pieces = JSON.parse(stringPieces); // pieces list
-let params = new URLSearchParams(window.location.search);
 let piece = pieces[params.get("id")] // piece that should currently display
 
 // Display the piece's name :
-var pieceNameTitle = document.createElement("h1");
+let pieceNameTitle = document.createElement("h1");
 pieceNameTitle.innerHTML = `${piece.name}`;
 document.getElementById('piece-infos').appendChild(pieceNameTitle);
 
+// Display a button to add a new version :
+let addVersionLinkButton = document.createElement('a');
+addVersionLinkButton.setAttribute('href',`new-version.html?id=${params.get("id")}`)
+addVersionLinkButton.innerHTML = "<button>Add new version</button>";
+document.getElementById('piece-infos').appendChild(addVersionLinkButton);
+
 // Display all the piece's versions :
 piece.versions.forEach(version => {
-    var versionDiv = document.createElement('div');
+    let versionDiv = document.createElement('div');
     versionDiv.innerHTML = `
         <h2>Version: ${version.number}</h2>
         <p>Date of creation: ${version.date}</p>
         <h2>Description:</h2>
         <p>${version.description}</p>
-    `
+    `;
     document.getElementById('piece-infos').appendChild(versionDiv);
 });
-
