7a44d9f33b35cdc126aa0f8daba48023f7fedb8a
Lucas-132 <lucas.pollet135@gmail.com>
2025-12-06 18:52:56 +0100

    View-piece buttons added on main page and redirect to a different url for each piece

--- js/list-pieces.js
+++ js/list-pieces.js
@@ -2,9 +2,15 @@ export function listPieces(){
     let stringPieces = localStorage.getItem("pieces");
     let pieces = JSON.parse(stringPieces);
 
-    pieces.forEach((piece) => {
+    pieces.forEach((piece, index) => {
         var pieceDiv = document.createElement('div');
-        pieceDiv.innerHTML = `<h2>${piece.name}</h2>`
+        pieceDiv.innerHTML = `
+            <h2>${piece.name}</h2>
+            <a href="piece.html?id=${index}">
+            <button>View Piece</button>
+            </a>
+        `
+        
         document.getElementById('pieces-display').appendChild(pieceDiv);
     });
 }
